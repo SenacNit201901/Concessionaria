35e7dcd116c350ac4860a7b742698fc7a0dd959e
Gabriel543 <gabriel_braga@id.uff.br>
2019-10-28 20:48:04 -0300

    Criação ServletLogin

---
+++ Concessionaria/src/br/com/senac/concessionaria/controle/ServletLogin.java
@@ -0,0 +1,29 @@
+package br.com.senac.concessionaria.controle;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletLogin")
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletLogin() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		// TODO Auto-generated method stub
+		doPost(request,response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email");
+		String senha = request.getParameter("senha");
+		
+	}
+
+}
